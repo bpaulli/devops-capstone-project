2288ed5d0d237a0fc60983110b582ce365cae858
bpaulli <bpaulli@hotmail.com>
2023-12-28 14:56:23 -0300

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -94,7 +94,7 @@ def read_account(account_id):
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
-    Actualiza una cuenta
+    Actualiza una cuenta si existe
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
